61dc4b09914691b6c057483cc3e08f9c01095920
Rolf Madsen <rolfrm@gmail.com>
2011-06-13 17:10:59 +0200

    Added math32 - functions to fast math using float32's

---
+++ math32.go
@@ -0,0 +1,11 @@
+package main
+import "math"
+func Sin32(x float32) float32{
+	return float32(math.Sin(float64(x)))
+}
+
+func Cos32(x float32) float32 {
+	return float32(math.Cos(float64(x)))
+}
+
+
