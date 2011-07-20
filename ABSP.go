effeadb063090cac378372c2d3bed640799b924c
Rolf Madsen <rolfrm@gmail.com>
2011-07-21 00:22:13 +0200

    Added Axis algined KD tree structure

---
+++ ABSP.go
@@ -0,0 +1,246 @@
+package main
+import "fmt"
+func SPMap(f func(SPData), set []SPData){
+	for i:=0; i < len(set);i++ {
+		f(set[i])
+	}
+}
+
+type ABSPNode struct{
+	Data []SPData
+	Split [2]*ABSPNode
+	splitDim int
+	splitPos float32
+	IsSplit bool
+	Root *ABSPNode
+}
+
+func (self *ABSPNode) Insert(obj SPData ){
+	self.Data = append(self.Data,obj)
+}
+
+
+func (self *ABSPNode) GetMean()(Vec3){
+	var out Vec3 = Vec3{0,0,0}
+	var n float32 = 0
+	SPMap(func(obj SPData){
+		out = out.Add(obj.GetPosition())
+		n += 1
+		
+	},self.Data)
+	return out.Scale(1/n)
+}
+
+func (self *ABSPNode) GetVariance()(Vec3){
+	var out Vec3 = Vec3{0,0,0}
+	var mean Vec3 = self.GetMean()
+	fmt.Println("Mean: " , mean)
+	SPMap(func(obj SPData){
+		out = out.Add(obj.GetPosition().Sub(mean).ElmPow(2))
+	},self.Data)
+	return out
+}
+
+func (self *ABSPNode) Divide(){
+	if len(self.Data) < 8 {
+		return
+	}
+	var mean Vec3 = self.GetMean()
+	var variance Vec3 = self.GetVariance()
+	var comp int = variance.BiggestComponent()
+	var test *ABSPNode = new(ABSPNode)
+	test.splitDim = comp
+	test.splitPos = mean.GetComponent(comp)
+	var caseCounter [3]int;
+	SPMap(func(obj SPData){
+		caseCounter[test.Cell(obj)] +=1
+	},self.Data)
+	fmt.Println(caseCounter)
+	if caseCounter[0] + caseCounter[1] > 0 {
+		self.IsSplit = true
+		self.splitDim = test.splitDim
+		self.splitPos = test.splitPos
+		
+		self.Split[0] = new(ABSPNode)
+		self.Split[1] = new(ABSPNode)
+		self.Split[0].Root = self.Root
+		self.Split[1].Root = self.Root
+		var newData []SPData
+		SPMap(func( obj SPData){
+			cell := self.Cell(obj)
+			if cell != 2 {
+				self.Split[cell].Insert(obj)
+			}else{
+				newData = append(newData, obj)
+			}
+		},self.Data)
+		self.Data = newData
+		self.Split[0].Divide()
+		self.Split[1].Divide()
+	}
+}
+
+func (self *ABSPNode) Cell(obj SPData) int{
+	var oSize float32 = obj.GetSize().GetComponent(self.splitDim)
+	var diff float32 = self.splitPos - obj.GetPosition().GetComponent(self.splitDim)
+	if Fabs32(diff) < oSize {
+		return 2 //both
+	}else if diff > 0 {
+		return 1
+	}
+	return 0
+}
+
+func (self *ABSPNode) findContainingChild(obj SPData)(*ABSPNode){
+	if self.IsSplit == false {
+		return self
+	}
+
+	cell := self.Cell(obj)
+	if cell == 2 {
+		return self
+	}
+	return self.Split[cell].findContainingChild(obj)
+
+}
+
+func (self *ABSPNode) Find(obj SPData)(index int){
+	for i:= 0; i < len(self.Data); i++ {
+		if self.Data[i] == obj {
+			return i
+		}
+	}
+	return -1
+}
+
+func (self *ABSPNode) RemoveObj(obj SPData){
+	objIndex := self.Find(obj)
+	if objIndex != -1 {
+		self.Data = append(self.Data[:objIndex],self.Data[objIndex+1:]...)
+	}
+}
+
+func (self *ABSPNode) Update(){
+	SPMap(func(obj SPData){
+		containingNode := self.Root.findContainingChild(obj)
+		if containingNode == self {
+			return
+		}
+		//fmt.Println("This happened")
+		self.RemoveObj(obj)
+		containingNode.Insert(obj)
+	},self.Data)
+	if self.IsSplit {
+		self.Split[0].Update()
+		self.Split[1].Update()
+	}
+}
+
+func (self *ABSPNode) Traverse(i int){
+	fmt.Println("recursion level: " , i)
+	if self.IsSplit {
+		self.Split[0].Traverse(i+1)
+		self.Split[1].Traverse(i+1)
+	}
+}
+
+func (self *ABSPNode) cd(fcd func(o1,o2 SPData)){
+	SPMap(func(nobj SPData){
+		self.cdo(fcd,nobj)
+	},self.Data)
+}
+
+func (self *ABSPNode) cdo(fcd func(o1,o2 SPData),obj SPData){
+	SPMap(func(nobj SPData){
+		fcd(obj,nobj)
+	},self.Data)
+	if self.IsSplit {
+		cell := self.Cell(obj)
+		if cell == 0 {
+			self.Split[0].cdo(fcd,obj)
+		}else if cell == 1{
+			self.Split[1].cdo(fcd,obj)
+		}else{
+			self.Split[1].cdo(fcd,obj)
+			self.Split[0].cdo(fcd,obj)
+		}
+	}
+}
+
+
+func (self *ABSPNode) RunCollisionFunction(colFunc func(o1 ,o2 SPData), obj SPData){
+	if obj != nil {
+		SPMap(func(nobj SPData){
+			colFunc(obj,nobj)
+		},self.Data)
+		if self.IsSplit {
+			cell := self.Cell(obj)
+			self.Split[cell].RunCollisionFunction(colFunc,obj)
+			//self.Split[1].RunCollisionFunction(colFunc,obj)
+		}
+	}
+	fmt.Println("len: ", len(self.Data))
+	for i:= 0; i< len(self.Data); i++ {
+		for j:=i+1; j < len(self.Data); j++ {
+			colFunc(self.Data[i],self.Data[j])
+		}
+		if self.IsSplit{
+			cell :=self.Cell(self.Data[i])
+			fmt.Println(cell)
+			if cell == 2 || cell == 0 {
+				self.Split[0].RunCollisionFunction(colFunc,self.Data[i])
+			}				
+			if cell ==2 || cell == 1 {
+				self.Split[1].RunCollisionFunction(colFunc,self.Data[i])
+			}
+			
+		}
+	}
+	
+}
+
+/*type testBox2 struct {
+	Vec3 Pos
+	Vec3 Size
+}
+
+func (self *testBox2) GetPosition()(Vec3){
+
+	return self.Pos
+}
+
+func (self *testBox) GetSize()(Vec3) {
+	return self.Size
+}*/
+
+
+func ABSPTest(){
+
+	absp := new(ABSPNode)
+	absp.Root = absp
+	var datalist []*testBox
+	for i:= float32(0); i < 10000; i++{
+		datalist = append(datalist,&testBox{Vec3{i/5,i/7,i/4},Vec3{1,1,1}})
+	}
+	for i:= 0; i < len(datalist);i++ {
+		absp.Insert(datalist[i])
+	}
+
+	fmt.Println(absp.GetMean())
+	fmt.Println(absp.GetVariance())
+	absp.Divide()
+	datalist[10].Pos =Vec3{0,0,0}
+	datalist[15].Pos = Vec3{100,10,0}
+	absp.Update()
+	absp.Update()
+	i  := 0
+	/*absp.RunCollisionFunction(func(obj1, obj2 SPData){
+		i += 1
+		
+	},nil)*/
+	absp.cd(func(obj1,boj2 SPData){
+		i +=  1
+	})
+	absp.Traverse(0)
+	fmt.Println(i)
+}
